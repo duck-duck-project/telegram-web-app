57d810216d0f85cfdc0b3782b27a111012a9a500
Eldos <eldos.baktybekov@gmail.com>
2024-01-07 21:38:58 +0600

    Add Telegram integration

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,7 @@ import { RouterView } from 'vue-router';
 import { provide } from 'vue';
 import GlobalNavbar from './components/GlobalNavbar.vue'
 
-// const userId = window.Telegram.WebApp
-const userId = 896678539
+const userId = window.Telegram.WebApp.initDataUnsafe.user.id
 
 provide('userId', userId)
 </script>
