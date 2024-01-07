ce9049449815f61d8dca04797d4bc3a7075068cd
Eldos <eldos.baktybekov@gmail.com>
2024-01-07 20:28:36 +0600

    💄  Add bootstrap icons

---
+++ src/components/GlobalNavbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary px-2">
+    <div class="container">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <RouterLink :to="{ name: 'contacts-list' }" class="nav-link" aria-current="page">Контакты</RouterLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
